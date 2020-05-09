78b5e3ed68f2ebe31e19452895318375eb0544b2
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-05-09 22:29:10 +0100

    ac-159: added can_show_non_standardised to the text viewer api reponse.

--- r.sh
+++ r.sh
@@ -1,2 +1,3 @@
-wget "http://10.0.2.2:8001/digipal/api/annotation/?_type=text&_image__id__in=[4,5]&@select=id,geo_json,image_id&@limit=1000" -O media/arch-annotations.json
-curl "http://10.0.2.2:8001/digipal/api/textcontentxml/?@select=*status,id,str,content,*text_content,*item_part,group,group_locus,type,*current_item,locus,shelfmark,*repository,place&@limit=10000" > arch-content.json
+# wget "http://10.0.2.2:8001/digipal/api/annotation/?_type=text&_image__id__in=[4,5]&@select=id,geo_json,image_id&@limit=1000" -O media/arch-annotations.json
+# curl "http://10.0.2.2:8001/digipal/api/textcontentxml/?@select=*status,id,str,content,*text_content,*item_part,group,group_locus,type,*current_item,locus,shelfmark,*repository,place&@limit=10000" > arch-content.json
+curl "http://localhost:8001/digipal/api/textcontentxml/?@select=*status,id,str,content,*text_content,*item_part,group,group_locus,type,*current_item,locus,shelfmark,*repository,place&@limit=10000" > cotr/arch-content.json
