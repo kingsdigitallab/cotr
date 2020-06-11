649e9f6d404a4ad4ad64ce9cd3615dbd16b5df1f
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-06-11 23:26:36 +0100

    Fix: temporary fix for the settings.WAGTAIL_SITE_NAME not being displaye properly in header.html.

--- cotr/utils/context_processors.py
+++ cotr/utils/context_processors.py
@@ -2,4 +2,8 @@ from django.conf import settings
 
 
 def settings_context(_request):
-    return {"settings": settings}
+    return {
+        "settings": settings,
+        # temporary fix as {{ settings.WAGTAIL_SITE_NAME }} doesn't work from templates
+        "WAGTAIL_SITE_NAME": settings.WAGTAIL_SITE_NAME,
+    }
