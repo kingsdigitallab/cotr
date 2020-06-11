ebacdf1d61fc7a871ff5b026c8c4873d9db8da3d
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-06-11 23:58:24 +0100

    Undoing the last quick fix as it's not really a proper solution, any settings access from the templates will have this problem.

--- cotr/utils/context_processors.py
+++ cotr/utils/context_processors.py
@@ -4,6 +4,4 @@ from django.conf import settings
 def settings_context(_request):
     return {
         "settings": settings,
-        # temporary fix as {{ settings.WAGTAIL_SITE_NAME }} doesn't work from templates
-        "WAGTAIL_SITE_NAME": settings.WAGTAIL_SITE_NAME,
     }
